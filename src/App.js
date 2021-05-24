6206598b6e2b4b434e835c6c90403fa97b1de918
Mary Ann Tackett <77631192+vectorbeat@users.noreply.github.com>
2021-05-24 11:46:44 -0500

    Import bootstrap and clean files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        className="form-control form-control-lg"
+        type="text"
+        placeholder=".form-control-lg"
+        aria-label=".form-control-lg example"
+      />
     </div>
   );
 }

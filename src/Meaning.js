98c6a3d621660858ef56148147a32fbdc5d5a483
Mary Ann Tackett <77631192+vectorbeat@users.noreply.github.com>
2021-05-25 12:34:00 -0500

    fix errors and update css

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,16 +3,16 @@ import React from "react";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <div class="card">
-        <div class="card-header">{props.meaning.partOfSpeech}</div>
-        <div class="card-body">
-          <h5 class="card-title definition">
+      <div className="card">
+        <div className="card-header">{props.meaning.partOfSpeech}</div>
+        <div className="card-body">
+          <h5 className="card-title definition">
             {props.meaning.definitions[0].definition}
           </h5>
-          <p class="card-text example">
+          <p className="card-text example">
             {props.meaning.definitions[0].example}
           </p>
-          <a href="#" class="btn btn-primary">
+          <a href="https://www.google.com" className="btn btn-primary">
             {props.meaning.definitions[0].synonyms}
           </a>
         </div>

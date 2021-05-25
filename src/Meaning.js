cc89ce4fa0772110673b16f5c02665fcaee45623
Mary Ann Tackett <77631192+vectorbeat@users.noreply.github.com>
2021-05-25 11:50:01 -0500

    Add card styling to meanings

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,16 +3,19 @@ import React from "react";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      <div className="definition">
-        {props.meaning.definitions[0].definition}
-      </div>
-      <div className="example">{props.meaning.definitions[0].example}</div>
-      <div className="synonyms">
-        SYNONYMS:{" "}
-        <ul>
-          <li>{props.meaning.definitions[0].synonyms}</li>
-        </ul>
+      <div class="card">
+        <div class="card-header">{props.meaning.partOfSpeech}</div>
+        <div class="card-body">
+          <h5 class="card-title definition">
+            {props.meaning.definitions[0].definition}
+          </h5>
+          <p class="card-text example">
+            {props.meaning.definitions[0].example}
+          </p>
+          <a href="#" class="btn btn-primary">
+            {props.meaning.definitions[0].synonyms}
+          </a>
+        </div>
       </div>
     </div>
   );

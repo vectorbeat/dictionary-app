fcbd5fe52fb7921b95ffcd2ab254e0385eb60176
Mary Ann Tackett <77631192+vectorbeat@users.noreply.github.com>
2021-05-26 17:20:57 -0500

    Add Phonetics component and begin styling phonetics comp

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h1>{props.results.word}</h1>
+        {props.results.phonetics.map(function (phonetics, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetic={phonetics} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>

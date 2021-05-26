89263f0e2d343a1dcb93865c1041f4fc374a71b3
Mary Ann Tackett <77631192+vectorbeat@users.noreply.github.com>
2021-05-26 08:24:04 -0500

    Add map function to definitions when multiples exist

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,12 +6,15 @@ export default function Meaning(props) {
       <div className="card">
         <div className="card-header">{props.meaning.partOfSpeech}</div>
         <div className="card-body">
-          <h5 className="card-title definition">
-            {props.meaning.definitions[0].definition}
-          </h5>
-          <p className="card-text example">
-            {props.meaning.definitions[0].example}
-          </p>
+          {props.meaning.definitions.map(function (definition, index) {
+            return (
+              <div className="card-title definition">
+                <div key={index}>{definition.definition}</div>
+                <p className="card-text example">{definition.example}</p>
+              </div>
+            );
+          })}
+
           <div className="links">{props.meaning.definitions[0].synonyms}</div>
         </div>
       </div>
